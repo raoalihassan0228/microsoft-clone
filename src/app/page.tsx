2abc87f8750fd31b35b96cc1fe4ace3280f7923a
Ali Hassan <139099370+raoalihassan0228@users.noreply.github.com>
2023-12-16 15:10:36 +0500

    Microsoft-clone-page
    This project is a clone of the Microsoft page created using HTML, CSS, and Tailwind CSS. Please note that this clone is for educational purposes only and should not be used for any malicious activities, including phishing attempts or deceiving users.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,110 @@
-import Image from 'next/image'
+import Image from "next/image";
+import AccountIco from "../app/assets/account icon.svg";
+import logo from "../app/assets/mslogo.png";
+import search from "../app/assets/search.svg";
+import ic1 from '../app/assets/ic1.png'
+import ic2 from '../app/assets/ic2.webp'
+import ic3 from '../app/assets/ic3.webp'
+import ic4 from '../app/assets/ic4.png'
+import cart from '../app/assets/cart icon.svg'
+import ms from '../app/assets/ms.webp'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <div className="container mx-auto">
+      <div className="navbar flex justify-between items-center p-4">
+        <div className="flex justify-center items-center  md:order-2">
+          <div className="hamburger inline-block p-4 cursor-pointer md:hidden">
+            <div className="line h-0.5 w-6 my-1 bg-black" />
+            <div className="line h-0.5 w-6 my-1 bg-black" />
+            <div className="line h-0.5 w-6 my-1 bg-black" />
+          </div>
+          <div className="search md:hidden w-4 mr-6">
+            <Image src={search} alt="" />
+          </div>
+        </div>
+        <div className="logo text-center flex md:order-1">
+          <div className="flex justify-start">
+            <Image className="w-[50%] cursor-pointer" src={logo} alt=""/>
+          </div>
+          <div className="features absolute w-fit md:static md:w-auto bg-gray-200 md:bg-white inset-0 md:flex md:items-center md:mx-4 md:space-x-6 -translate-x-96 md:translate-x-0">
+            <div className="fitem hover:underline hover:underline-offset-8 cursor-pointer">
+              Microsoft 365
+            </div>
+            <div className="hover:underline hover:underline-offset-8 cursor-pointer">
+              Office
+            </div>
+            <div className="hover:underline hover:underline-offset-8 cursor-pointer">
+              Copilot
+            </div>
+            <div className="hover:underline hover:underline-offset-8 cursor-pointer">
+              Windows
+            </div>
+            <div className="hover:underline hover:underline-offset-8 cursor-pointer">
+              Xbox
+            </div>
+            <div className="hover:underline hover:underline-offset-8 cursor-pointer">
+              Support
+            </div>
+          </div>
+        </div>
+        <div className="cart text-center md:order-3 flex">
+          <div className="search hidden md:block mr-6 cursor-pointer hover:underline"> All Microsoft </div>
+          <div className="mx-2 cursor-pointer">Search</div>
+          <Image className="w-4 mr-6 cursor-pointer" src={search} alt="" />
+          <div className="flex pt-1">
+            <Image className="w-4 h-4 mr-4 cursor-pointer" src={cart} alt="" />
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              className="w-4 h-4 mr-4 cursor-pointer"
+              src={AccountIco}
+              alt=""
             />
-          </a>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="slider flex flex-col-reverse md:flex-row bg-[#f2f2f2]">
+        <div className="left flex flex-col justify-center items-center md:items-baseline py-12 ml-32 space-y-5">
+          <h1 className="text-2xl font-medium md:text-4xl mx-5">
+            Surface Laptop 4
+          </h1>
+          <p className="w-3/4 mx-5 text-center md:text-left">
+            Do it all with a perfect balance of sleek, ultra-thin design,
+            multitasking speed and improved performance
+          </p>
+          <button className="text-white bg-black px-4 py-2 my-6 font-bold mx-5">
+            Shop Now &gt;
+          </button>
+        </div>
+        <div className="right">
+          <Image className="w-[60rem] md:w-[64rem]" src={ms} alt="" />
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="promo space-y-6 flex flex-col lg:flex-row justify-center shadow-md text-blue-600 ">
+        <div className="item flex items-center mx-4 mt-6 space-x-2 md:flex-col">
+          <Image className="cursor-pointer" src={ic1} alt="" />
+          <span className="font-semibold md:w-[8rem] md:text-center text-sm my-4 underline hover:cursor-pointer">
+            Choose your Microsoft 365
+          </span>
+        </div>
+        <div className="item flex md:flex-col items-center mx-4 space-x-2">
+          <Image className="cursor-pointer" src={ic2} alt="" />
+          <span className="font-semibold md:w-[8rem] md:text-center text-sm my-4 underline hover:cursor-pointer">
+            Explore Surface devices
+          </span>
+        </div>
+        <div className="item flex items-center mx-4 space-x-2 md:flex-col">
+          <Image className="cursor-pointer" src={ic3} alt="" />
+          <span className="font-semibold md:w-[8rem] md:text-center text-sm my-4 underline hover:cursor-pointer">
+            Buy Xbox games
+          </span>
+        </div>
+        <div className="item flex items-center mx-4 space-x-2 md:flex-col">
+          <Image className="cursor-pointer" src={ic4} alt="" />
+          <span className="font-semibold md:w-[8rem] md:text-center text-sm my-4 underline hover:cursor-pointer">
+            Get Windows 11
+          </span>
+        </div>
       </div>
-    </main>
-  )
+    </div>
+  );
 }
